b89c698cadf890703b11ceee90ae0ee61a98b7b5
Belkis <katerinne65@gmail.com>
2022-04-29 00:04:27 +0200

    Clase 368 Servir una página HTTP desde Express

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const cors = require('cors');
-require("dotenv").config();
+require('dotenv').config();
 
 console.log(process.env);
 
 // Crear el servidor/aplicacion de express
 const app = express();
+
 
 //Lectura y parseo del body
 app.use( express.json() );
@@ -13,8 +14,12 @@ app.use( express.json() );
 //Rutas
 app.use('/api/auth', require('./routes/auth'))
 
+
 //CORS
 app.use(cors());
+
+//Directorio publico
+app.use(express.static('./public') );
 
 
 app.listen(process.env.PORT, () => {
