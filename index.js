01f344151ffdb33ba3375985fcbdfe0d38305293
Belkis <katerinne65@gmail.com>
2022-04-28 23:14:49 +0200

    Clase 365 Separar el controlador de la ruta

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+// Crear el servidor/aplicacion de express
+const app = express();
+
+//Rutas
+app.use('/api/auth', require('./routes/auth'))
+
+
+app.listen(4000, () => {
+    console.log(`Servidor corriendo en puerta ${ 4000 }`);
+});
---
+++ routes/auth.js
@@ -0,0 +1,17 @@
+const { Router } = require('express');
+const { crearUsuario, loginUsuario, revalidarToken } = require('../controllers/auth');
+
+const router = Router();
+
+
+// Crear un nuevo usuario
+router.post('/new', crearUsuario)
+
+// Login de usuario
+router.post('/', loginUsuario)
+
+//Validar y revalidar Token
+router.get('/renew', revalidarToken)
+
+
+module.exports = router;
