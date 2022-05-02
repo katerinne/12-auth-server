e1cbf7d501449f7d2042e4704c0b844db6025647
Belkis <katerinne65@gmail.com>
2022-05-02 17:33:31 +0200

    Clase 376 Hash de la contraseña

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const { response }  = require('express');
 const Usuario = require('../models/Usuario');
+const bcrypt = require('bcryptjs');
 
 const crearUsuario = async(req, res = response) => {
    
@@ -18,9 +19,11 @@ const crearUsuario = async(req, res = response) => {
         }
 
         // Crear usuario con el modelo
-        const dbUser = new Usuario( req.body);             
-        //Hashear la contraseña
+        const dbUser = new Usuario( req.body);         
 
+        //Hashear la contraseña
+        const salt = bcrypt.genSaltSync();
+        dbUser.password = bcrypt.hashSync(password, salt);
 
         // Generar el JWT
 
